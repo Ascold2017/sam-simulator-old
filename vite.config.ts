6f5d1d07ce7d383d4830753a44734e639c5bcc63
Mihail <mihail.pogosyan96@gmail.com>
2022-08-01 19:15:45 +0300

    Optimize build; Encode sound to bas64 string

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vuetify from 'vite-plugin-vuetify';
 import { viteSingleFile } from "vite-plugin-singlefile"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify(), viteSingleFile({ removeViteModuleLoader: true, useRecommendedBuildConfig: false })],
+  plugins: [vue(), vuetify({ autoImport: true }),  viteSingleFile({ removeViteModuleLoader: true, useRecommendedBuildConfig: false })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
