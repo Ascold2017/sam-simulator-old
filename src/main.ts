4631063a9c93dcecc93afb0ee5142447386021dc
Mihail <mihail.pogosyan96@gmail.com>
2022-08-02 17:38:23 +0300

    Fix build

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import { createVuetify } from "vuetify";
+import * as components from 'vuetify/components'
 (window as any).__ACCELERATION__ = 1;
 const vuetify = createVuetify({
+  components,
   theme: {
     defaultTheme: "dark",
   },
